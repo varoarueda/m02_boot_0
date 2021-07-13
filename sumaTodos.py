ca941be41912cc15e496b09999263cc310fd1eb3
Usuario principal <UsuarioPrincipal@MacBook-Pro-de-adrian.local>
2021-07-13 22:14:08 +0200

    Funciones Clásicas

--- sumaTodos.py
+++ sumaTodos.py
@@ -4,5 +4,15 @@ def sumaTodos(limitTo):
         resultado += i
     return resultado
 
+def sumaTodosLosCuadrados(limitTo):
+    resultado = 0
+    for i in range(limitTo+1):
+        resultado += i*i
+        
+    return resultado
+
 print(sumaTodos(100))
+print(sumaTodosLosCuadrados(3))
+
+        
     
