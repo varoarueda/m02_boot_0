dc3515a040d8e7938b612058c1448d4383473be9
Usuario principal <UsuarioPrincipal@MacBook-Pro-de-adrian.local>
2021-07-14 18:47:16 +0200

    Tortuga

---
+++ objetos.py
@@ -0,0 +1,11 @@
+import turtle
+
+tortuguita = turtle.Turtle()
+otraTortuga = turtle.Turtle()
+
+tortuguita.shape("turtle")
+otraTortuga.color("green")
+tortuguita.fd(50)
+otraTortuga.left(90)
+otraTortuga.fd(50)
+
