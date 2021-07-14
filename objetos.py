dff910969b44e08149f07d90d57ed41b95ff234a
Usuario principal <UsuarioPrincipal@MacBook-Pro-de-adrian.local>
2021-07-14 21:01:16 +0200

    objetos.py

--- objetos.py
+++ objetos.py
@@ -2,10 +2,15 @@ import turtle
 
 tortuguita = turtle.Turtle()
 otraTortuga = turtle.Turtle()
+lentorra = turtle.Turtle()
 
+lentorra.speed(1)
 tortuguita.shape("turtle")
 otraTortuga.color("green")
+tortuguita.color("blue")
 tortuguita.fd(50)
 otraTortuga.left(90)
 otraTortuga.fd(50)
+lentorra.right(90)
+lentorra.fd(50)
 
