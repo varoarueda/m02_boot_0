bba408b7f45ef47bd75405c70c70ad5502eec748
Usuario principal <UsuarioPrincipal@MacBook-Pro-de-adrian.local>
2021-07-14 23:02:46 +0200

    perro 2 version

--- mi primer objeto perro.py
+++ mi primer objeto perro.py
@@ -4,3 +4,15 @@ class Perro():
         self.edad = edad
         self.peso = peso
         
+    def ladrar(self):
+        if self.peso >= 8:
+            print("GUAU, GUAU")
+        else:
+            print("guau, guau")
+            
+            
+    def __str__(self):
+        return "Perro {}, edad: {}, peso: {}".format(self.nombre, self.edad, self.peso)
+    
+        
+        
